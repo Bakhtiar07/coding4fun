8e3615ac67a0eec1c925bad09c3b4889ee5d9a9a
Bakhtiar07 <bmh46353@uga.edu>
2024-04-07 18:19:53 -0400

    working on weather api

--- #003__ Discord bot/main.py
+++ #003__ Discord bot/main.py
@@ -1,19 +1,27 @@
 from typing import Final
 import os
+import discord
+import requests
 from dotenv import load_dotenv
+from discord.ext import commands
 from discord import Intents, Client, Message
-from weather_api import get_bot
 from simple_responses import get_response
 from interactive_responses import start_timer, pause_timer, resume_timer, cancel_timer
 
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+api_key: Final[str] = os.getenv('API_KEY')
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
 
 # STEP 1: BOT SETUP
-intents: Intents = Intents.default()
+intents: Intents = discord.Intents.default()
 intents.message_content = True  # NOQA
+intents.message_content = True
 client: Client = Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 # STEP 2: MESSAGE FUNCTIONALITY
@@ -33,12 +41,50 @@ async def send_message(message: Message, user_message: str) -> None:
 
 
 # STEP 3: HANDLING THE STARTUP FOR OUR BOT
-@client.event
+@client.event 
 async def on_ready() -> None:
     print(f'Logged in as {client.user}. Ready to serve!')
+    
+@bot.event
+async def on_ready() -> None:
+    print(f'Logged in as{bot.user}.')
 
 
 # STEP 4: HANDLING INCOMING MESSAGES
+
+@bot.command()
+async def weather(ctx, city: str):
+    city_name = city
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    #channel = ctx.message.channel
+    
+    #Check if city name is valid
+    if x["cod"] != "404":
+        #async with channel.typing():
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis = str(round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            
+            embed = discord.Embed(title=f"Weather in {city_name}",
+                              color=ctx.guild.me.top_role.color,
+                              timestamp=ctx.message.created_at,)
+            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            
+            await ctx.send(embed=embed)
+    else:
+            await ctx.send(f"There was no results about this place!")
+
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user:
@@ -66,8 +112,8 @@ async def on_message(message: Message) -> None:
 
 # STEP 5: MAIN ENTRY POINT
 def main() -> None:
+    bot.run(token=TOKEN)
     client.run(token=TOKEN)
-    get_bot.run(token = TOKEN)
 
 
 if __name__ == '__main__':
--- #003__ Discord bot/weather_api.py
+++ #003__ Discord bot/weather_api.py
@@ -1,15 +1,23 @@
 import requests
 import discord
 from discord.ext import commands
+from discord import Intents, Client, Message
+from dotenv import load_dotenv
+from typing import Final
+import os
 
-intents = discord.Intents.default()
-intents.messages = True
+
+intents: Intents = Intents.default()
+intents.message_content = True  # NOQA
+client: Client = Client(intents=intents)
+
 
 bot = commands.Bot(command_prefix='!', intents=intents) # Choose a prefix
-api_key = "56bd6af83e5488abdc76f204024a0e31"
+load_dotenv()
+api_key: Final[str] = os.getenv('API_KEY')
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-@bot.command()
+@client.command()
 async def weather(ctx, *, city: str):
     city_name = city
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
@@ -27,6 +35,7 @@ async def weather(ctx, *, city: str):
             current_humidity = y["humidity"]
             z = x["weather"]
             weather_description = z[0]["description"]
+            
             embed = discord.Embed(title=f"Weather in {city_name}",
                               color=ctx.guild.me.top_role.color,
                               timestamp=ctx.message.created_at,)
@@ -36,6 +45,8 @@ async def weather(ctx, *, city: str):
             embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
             embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
             embed.set_footer(text=f"Requested by {ctx.author.name}")
+            
+            await channel.send(embed=embed)
+    else:
+            await channel.send(f"There was no results about this place!")
 
-def get_bot():
-    return bot
